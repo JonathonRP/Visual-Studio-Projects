40550ff32c28b21a747e40bf15c61960ca7a80de
Jonathon Reese Perry <jreesep@gmail.com>
2019-01-15 15:55:09 -0600

    Update App.xaml.cs

--- CalorieCountApp/CalorieCountApp/CalorieCountApp/App.xaml.cs
+++ CalorieCountApp/CalorieCountApp/CalorieCountApp/App.xaml.cs
@@ -15,22 +15,22 @@ namespace CalorieCountApp
 		{
 			InitializeComponent();
 
-            MainPage = new CalorieCountApp.MainPage();
-        }
+            		MainPage = new CalorieCountApp.MainPage();
+        	}
 
 		protected override void OnStart ()
 		{
-            // Handle when your app starts
-        }
+		    // Handle when your app starts
+		}
 
 		protected override void OnSleep ()
 		{
-            // Handle when your app sleeps
-        }
+		    // Handle when your app sleeps
+		}
 
 		protected override void OnResume ()
 		{
-            // Handle when your app resumes
-        }
+		    // Handle when your app resumes
+		}
 	}
 }
